b0d2ba5f98e3fbfd7d67a3f413b63e0401341a02
szymqn <koodziejskisz@outlook.com>
2024-11-16 22:51:00 +0100

    Added plot result method & vizualized methods rate

--- lab06/main.py
+++ lab06/main.py
@@ -1,5 +1,6 @@
 import random
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def initialize_population(pop_size, dim):
@@ -56,31 +57,60 @@ def genetic_algorithm(dim, pop_size, pk):
 
 
 def t_01():
+    results = []
     for epoch in N_EPOCH:
         population = initialize_population(epoch, DIM)
 
         for p in PM:
             mutations = n_dimensional_mutation(population, pm=p)
+            mutations_rate = mutations / epoch * 100
 
-            print(f"Mutation rate, {epoch}/{p}:", mutations/epoch*100)
+            print(f"Mutation rate, {epoch}/{p}:", mutations_rate)
+            results.append((epoch, p, mutations_rate))
+
+    return results
 
 
 def t_02():
+    results = []
     for epoch in N_EPOCH:
         population = initialize_population(epoch, DIM)
 
         for p in PM:
-            mutations = n_dimensional_inversion(population, pm=p)
+            inversions = n_dimensional_inversion(population, pm=p)
+            inversions_rate = inversions / epoch * 100
 
-            print(f"Mutation rate, {epoch}/{p}:", mutations / epoch * 100)
+            print(f"Mutation rate, {epoch}/{p}:", inversions_rate)
+            results.append((epoch, p, inversions_rate))
+
+    return results
 
 
 def t_03():
+    results = []
     for epoch in N_EPOCH:
         for p in PM:
             cross = genetic_algorithm(DIM, epoch, p)
+            cross_rate = cross / epoch * 100
 
-            print(f'Cross rate, {epoch}/{p}:', cross/epoch*100)
+            print(f'Cross rate, {epoch}/{p}:', cross_rate)
+            results.append((epoch, p, cross_rate))
+    return results
+
+
+def plot_results(results, title, ylabel):
+    epochs = sorted(set([r[0] for r in results]))
+    pm_values = sorted(set([r[1] for r in results]))
+
+    for pm in pm_values:
+        rates = [r[2] for r in results if r[1] == pm]
+        plt.plot(epochs, rates, label=f'pm={pm}')
+
+    plt.xlabel('Epochs')
+    plt.ylabel(ylabel)
+    plt.title(title)
+    plt.legend(loc='upper right')
+    plt.show()
 
 
 if __name__ == '__main__':
@@ -88,6 +118,10 @@ if __name__ == '__main__':
     N_EPOCH = [10, 50, 100, 200, 500, 1000]
     PM = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
 
-    t_01()
-    t_02()
-    t_03()
+    t_01_results = t_01()
+    t_02_results = t_02()
+    t_03_results = t_03()
+
+    plot_results(t_01_results, 'Mutation Rates', 'Mutation Rate (%)')
+    plot_results(t_02_results, 'Inversion Rates', 'Inversion Rate (%)')
+    plot_results(t_03_results, 'Crossover Rates', 'Crossover Rate (%)')
